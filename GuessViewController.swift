1e241644da14525d4dbba36f4a80fd03cadb091e
tsaenogard <tsaenogard@gmail.com>
2017-05-16 04:00:45 +0800

    5/16 0400

--- GuessViewController.swift
+++ GuessViewController.swift
@@ -39,13 +39,91 @@ class GuessViewController: UIViewController {
             UIColor(red: 210 / 255, green: 105 / 255, blue: 30 / 255, alpha: 0.5)
         self.navigationController?.navigationBar.isTranslucent = false
         let rightButton = UIBarButtonItem(
-            title:"設定",
+            title:"說明",
             style:.plain,
             target:self,
             action:#selector(GuessViewController.explain))
         self.navigationItem.rightBarButtonItem = rightButton
         
         self.bombImageView = UIImageView(image: UIImage(named: "img1.jpg"))
+        self.bombImageView.contentMode = .scaleAspectFill
+        self.bombImageView.backgroundColor = UIColor.red
+        self.view.addSubview(self.bombImageView)
+        
+        for _ in 0 ..< numOfNum {
+            let textField = UITextField()
+            textField.placeholder = "_"
+            textField.font = UIFont(name: "Helvetica", size: 30)
+            textField.adjustsFontSizeToFitWidth = true
+            textField.textAlignment = .center
+            textField.inputView = UIView(frame: CGRect.zero)
+            textField.borderStyle = .line
+            textNums.append(textField)
+            self.view.addSubview(textField)
+        }
+        
+        for i in 0 ... 9 {
+            let button = UIButton()
+            button.setTitle("\(i)", for: .normal)
+            button.setTitleColor(UIColor.white, for: .normal)
+            button.backgroundColor = UIColor(red: 0, green: 127 / 255 , blue: 255 / 255, alpha: 1.0)
+            button.layer.cornerRadius = 5.0
+            button.addTarget(self, action: #selector(onBtnAction(_:)), for: .touchUpInside)
+            self.numBtn.append(button)
+            self.view.addSubview(button)
+        }
+        
+        self.checkBtn = UIButton()
+        self.checkBtn.setTitle("CHECK!", for: .normal)
+        self.checkBtn.setTitleColor(UIColor.white, for: .normal)
+        self.checkBtn.backgroundColor = UIColor.red
+        self.checkBtn.layer.cornerRadius = 5.0
+        self.checkBtn.addTarget(self, action: #selector(onCheckAction(_:)), for: .touchUpInside)
+        self.view.addSubview(self.checkBtn)
+        
+        self.resultTableView = UITableView()
+        self.resultTableView.delegate = self
+        self.resultTableView.dataSource = self
+        self.resultTableView.backgroundColor = UIColor(red: 252 / 255, green: 243 / 255, blue: 218 / 255, alpha: 1.0)
+        self.view.addSubview(self.resultTableView)
+        
+        textNums[0].becomeFirstResponder()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let gap: CGFloat = 8
+        self.bombImageView.frame.size = CGSize(width: width / 2, height: height / 6)
+        self.bombImageView.center = CGPoint(x: width / 2, y: height / 12)
+        
+        for (i,textField) in textNums.enumerated() {
+            textField.frame.size = CGSize(
+                width: height / 15,
+                height: height / 15)
+            
+            textField.center = CGPoint(
+                x: width * CGFloat(i + 1) / CGFloat(textNums.count + 1),
+                y: self.bombImageView.frame.maxY + height / 30 + gap)}
+        
+        for (i,button) in numBtn.enumerated() {
+            button.frame.size = CGSize(
+                width: height / 18,
+                height: height / 18)
+            button.center = CGPoint(
+                x: width / CGFloat(8) * CGFloat(i > 4 ? i - 4 : i + 1),
+                y: self.textNums[0].frame.maxY + height / 36 + gap + (i > 4 ? height / 18 + gap : 0))
+        }
+        
+        self.checkBtn.frame.size = CGSize(width: height / 9, height: 50)
+        self.checkBtn.center = CGPoint(x: width * 13 / 16, y: self.numBtn[0].frame.maxY + gap / 2)
+        
+        self.resultTableView.frame = CGRect(
+            x: width / 6,
+            y: self.numBtn[9].frame.maxY + gap,
+            width: width * 2 / 3,
+            height: height - self.numBtn[9].frame.maxY - (navigationController?.navigationBar.frame.height)! - 6 * gap)
+        print(resultTableView.frame.minY)
+        print(resultTableView.frame.height)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,13 +136,14 @@ class GuessViewController: UIViewController {
         var baseArray = [0,1,2,3,4,5,6,7,8,9]
         var resultArray: [Int] = []
         
-        for i in 0 ..< numOfNum {
+        for _ in 0 ..< numOfNum {
             let randon = Int(arc4random_uniform(UInt32(baseArray.count)))
             resultArray.append(baseArray[randon])
             if !boolOfRepeat {
                 baseArray.remove(at: randon)
             }
         }
+        print(ansNum)
         return resultArray
     }
     
@@ -73,19 +152,27 @@ class GuessViewController: UIViewController {
         var numCorrect = 0
         var repeatNum = 0
         for (ig, guess) in guessNum.enumerated() {
+            var _posCOrrect = 0
+            var _numCorrect = 0
             for (ia, ans) in ansNum.enumerated() {
-                var _posCOrrect = 0
-                var _numCorrect = 0
                 if guess == ans && ig == ia {
                     _posCOrrect += 1
                 }else if guess == ans {
                     _numCorrect += 1
                 }
-                
-
             }
+            if _posCOrrect + _numCorrect > 1 {
+                if posCorrect == 1 {
+                    repeatNum += 1
+                    _numCorrect = 0
+                }else {
+                    repeatNum += 1
+                    _numCorrect = 1
+                }
+            }
+            posCorrect += _posCOrrect
+            numCorrect += _numCorrect
         }
-        
         return (posCorrect, numCorrect, repeatNum)
     }
     
@@ -101,6 +188,21 @@ class GuessViewController: UIViewController {
         
     }
     //MARK: - Button
+    func onBtnAction(_ sender: UIButton) {
+        for (i,textField) in textNums.enumerated() {
+            if textField.isFirstResponder {
+                textField.text = sender.currentTitle
+                if i < textNums.count - 1 {
+                    textNums[i + 1].becomeFirstResponder()
+                }
+                return
+            }
+        }
+    }
+    
+    func onCheckAction(_ sender: UIButton) {
+        
+    }
     
 
     /*
@@ -113,4 +215,45 @@ class GuessViewController: UIViewController {
     }
     */
 
+}
+
+
+extension GuessViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultString.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        cell.textLabel?.textAlignment = .center
+        cell.textLabel?.font = UIFont(name: "Helvetica", size: 20)
+        cell.backgroundColor = UIColor(red: 252 / 255, green: 243 / 255, blue: 218 / 255, alpha: 1.0)
+        cell.textLabel?.text = resultString[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.frame =  CGRect(x: 0, y: 0, width: tableView.frame.width, height: 30)
+        label.backgroundColor = UIColor.lightGray
+        label.font = UIFont(name: "Helvetica", size: 20)
+        label.text = " 數字      結果 "
+        label.textAlignment = .center
+        
+        
+        return label
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 30
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 30
+    }
 }
--- SettingViewController.swift
+++ SettingViewController.swift
@@ -9,16 +9,112 @@
 import UIKit
 
 class SettingViewController: UIViewController {
+    let width = UIScreen.main.bounds.width
+    let height = UIScreen.main.bounds.height
+    
+    var titleImageView: UIImageView!
+    var numStackView: UIStackView!
+    var repeatStackView: UIStackView!
+    var goBtn: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        //導覽列設定
+        self.view.backgroundColor = UIColor.white
+        self.title = "Setting"
+        self.navigationController?.navigationBar.barTintColor =
+            UIColor(red: 210 / 255, green: 105 / 255, blue: 30 / 255, alpha: 0.5)
+        self.navigationController?.navigationBar.tintColor = UIColor.darkGray
+        self.navigationController?.navigationBar.isTranslucent = false
+        let rightButton = UIBarButtonItem(
+            title:"說明",
+            style:.plain,
+            target:self,
+            action:#selector(SettingViewController.explain))
+        self.navigationItem.rightBarButtonItem = rightButton
+        
+        self.titleImageView = UIImageView(image: UIImage(named: "title"))
+        self.titleImageView.contentMode = .scaleAspectFit
+        self.view.addSubview(self.titleImageView)
+        
+        self.numStackView = UIStackView()
+        let slider = UISlider()
+        slider.maximumValue = 6.0
+        slider.minimumValue = 4.0
+        slider.value = 4.0
+        slider.addTarget(self, action: #selector(onSlider(_:)), for: .valueChanged)
+        self.numStackView.addArrangedSubview(slider)
+        let numLabel = UILabel()
+        numLabel.text = "4"
+        numLabel.textColor = UIColor.darkGray
+        numLabel.font = UIFont(name: "Helvetica", size: 30)
+        self.numStackView.addArrangedSubview(numLabel)
+        self.numStackView.axis = .horizontal
+        self.numStackView.spacing = 20
+        self.numStackView.alignment = .fill
+        self.view.addSubview(self.numStackView)
+        
+        self.repeatStackView = UIStackView()
+        let repeatLabel = UILabel()
+        repeatLabel.text = "重複"
+        repeatLabel.textColor = UIColor.darkGray
+        repeatLabel.font = UIFont(name: "Helvetica", size: 26)
+        self.repeatStackView.addArrangedSubview(repeatLabel)
+        let repeatSwitch = UISwitch()
+        repeatSwitch.isOn = false
+        self.repeatStackView.addArrangedSubview(repeatSwitch)
+        self.repeatStackView.axis = .horizontal
+        self.repeatStackView.spacing = 20
+        self.repeatStackView.alignment = .fill
+        self.view.addSubview(self.repeatStackView)
+        
+        self.goBtn = UIButton()
+        self.goBtn.layer.borderWidth = 1.0
+        self.goBtn.layer.cornerRadius = 8.0
+        self.goBtn.setTitle("GO!", for: .normal)
+        self.goBtn.setTitleColor(UIColor.blue, for: .normal)
+        self.goBtn.addTarget(self, action: #selector(onGoBtn(_:)), for: .touchUpInside)
+        self.view.addSubview(self.goBtn)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let gap: CGFloat = 30
+        self.titleImageView.frame.size = CGSize(width: width * 2 / 3, height: height / 4 + gap)
+        self.titleImageView.center = CGPoint(x: width / 2, y: height / 8)
+        
+        self.numStackView.frame.size = CGSize(width: width / 3, height: 30)
+        self.numStackView.center = CGPoint(x: width / 2, y: self.titleImageView.frame.maxY + gap * 2)
+        
+        self.repeatStackView.frame.size = CGSize(width: width / 3, height: 30)
+        self.repeatStackView.center = CGPoint(x: width / 2, y: self.numStackView.frame.maxY + gap * 2)
+        
+        self.goBtn.frame.size = CGSize(width: 80, height: 50)
+        self.goBtn.center = CGPoint(x: width / 2, y: self.repeatStackView.frame.maxY + gap * 3)
+        print("\(goBtn.frame.maxY)/ \(height)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func explain() {
+        let pageViewController = ExplainPageController()
+        self.present(pageViewController, animated: true, completion: nil)
+    }
+    
+    func onGoBtn(_ sender: UIButton) {
+        let goViewController = GuessViewController()
+        goViewController.numOfNum = Int((numStackView.subviews[0] as! UISlider).value)
+        goViewController.boolOfRepeat = (repeatStackView.subviews[1] as! UISwitch).isOn
+        
+        self.navigationController?.pushViewController(goViewController, animated: true)
+    }
+    
+    func onSlider(_ sender: UISlider) {
+        (numStackView.subviews[1] as! UILabel).text = String(Int(sender.value))
     }
     
 
